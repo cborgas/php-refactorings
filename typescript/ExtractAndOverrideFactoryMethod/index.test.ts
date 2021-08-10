7ee6841b3ac27c5a25351e850636b0d99daf5652
Edgar <edgaryp@me.com>
2021-08-10 14:09:25 +1000

    chore: prettier update

--- typescript/ExtractAndOverrideFactoryMethod/index.test.ts
+++ typescript/ExtractAndOverrideFactoryMethod/index.test.ts
@@ -1,5 +1,5 @@
-import { testing } from "./deps.ts";
-import { App } from "./index.ts";
+import { testing } from './deps.ts';
+import { App } from './index.ts';
 
 const { assertEquals } = testing;
 
@@ -13,16 +13,16 @@ namespace RunTime {
   export class PaymentGatewayTest {
     public regression(): void {
       const paymentGateway = new RunTime.PaymentGateway();
-      paymentGateway.makePayment("Me", "You", 100);
+      paymentGateway.makePayment('Me', 'You', 100, 'AUD');
       const lodgedPayment = paymentGateway.getLedger().getPayment(0);
 
-      Deno.test("must return the payer name", (): void => {
-        assertEquals("Me", lodgedPayment.getPayer().getName());
+      Deno.test('must return the payer name', (): void => {
+        assertEquals('Me', lodgedPayment.getPayer().getName());
       });
-      Deno.test("must return the payee name", (): void => {
-        assertEquals("You", lodgedPayment.getPayee().getName());
+      Deno.test('must return the payee name', (): void => {
+        assertEquals('You', lodgedPayment.getPayee().getName());
       });
-      Deno.test("must return the cents", (): void => {
+      Deno.test('must return the cents', (): void => {
         assertEquals(100, lodgedPayment.getCents());
       });
     }
